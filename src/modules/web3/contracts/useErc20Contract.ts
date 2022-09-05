ce638fa86af78c2e61d2db32aefd5aea08541587
Hiep Nguyen Van <hiep.nguyen2@sotatek.com>
2022-09-05 18:37:51 +0700

    feat: init source code base

---
+++ src/modules/web3/contracts/useErc20Contract.ts
@@ -0,0 +1,7 @@
+import Erc20ABI from '@web3/abis/erc20.json';
+import { Erc20 } from '../abis/types';
+import { useContract } from './useContract';
+
+export const useErc20Contract = (address: string): Erc20 | null => {
+	return useContract<Erc20>(Erc20ABI, address);
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return <h1 className='text-center font-bold'>Landing page</h1>;
+};
+
+export default Home;
