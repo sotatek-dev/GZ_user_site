5566002e091aaac4384da149c938b2d2fb282246
trong.vu <trong.vu@sotatek.com>
2022-09-21 14:24:21 +0700

    feat: implement api to statistic section

--- src/modules/landing/components/Statistic.tsx
+++ src/modules/landing/components/Statistic.tsx
@@ -1,39 +1,75 @@
-import React from 'react';
+import { getStatistics } from 'apis/landing';
+import { numberWithDot } from 'common/helpers/number';
+import React, { useEffect, useMemo, useState } from 'react';
 import styles from '../style/statistic.module.scss';
-export default function Statistic() {
-	const totalSupply = '438.752.067';
-	const currentSupply = '64.155.460';
-	const tokensBurned = '61.247.933';
-	const totalHolders = '2.626';
 
-	const statisticBoxes = [
-		{
-			label: 'Total Supply',
-			value: totalSupply,
-			icon: '/icons/statistic_1.svg',
-		},
-		{
-			label: 'Current Supply',
-			value: currentSupply,
-			icon: '/icons/statistic_2.svg',
-		},
-		{
-			label: 'Tokens Burned',
-			value: tokensBurned,
-			icon: '/icons/statistic_3.svg',
-		},
-		{
-			label: 'Total Holders',
-			value: totalHolders,
-			icon: '/icons/statistic_4.svg',
-		},
-	];
+interface IStatistic {
+	totalSupply: number;
+	currentSupply: number;
+	tokensBurned: number;
+	totalHolders: number;
+}
+
+export default function Statistic() {
+	const defaultData: IStatistic = {
+		totalSupply: 0,
+		currentSupply: 0,
+		tokensBurned: 0,
+		totalHolders: 0,
+	};
+	const [statistic, setStatistic] = useState<IStatistic>(defaultData);
+
+	useEffect(() => {
+		const getStatistic = async () => {
+			const [data, err] = await getStatistics();
+
+			if (err) {
+				setStatistic(defaultData);
+			} else {
+				const { current_supply, total_burned, total_holders, total_supply } =
+					data.data;
+				setStatistic({
+					currentSupply: current_supply,
+					totalSupply: total_supply,
+					tokensBurned: total_burned,
+					totalHolders: total_holders,
+				});
+			}
+		};
+		getStatistic();
+	}, []);
+
+	const statisticBoxes = useMemo(() => {
+		return [
+			{
+				label: 'Total Supply',
+				value: statistic.totalSupply,
+				icon: '/icons/statistic_1.svg',
+			},
+			{
+				label: 'Current Supply',
+				value: statistic.currentSupply,
+				icon: '/icons/statistic_2.svg',
+			},
+			{
+				label: 'Tokens Burned',
+				value: statistic.tokensBurned,
+				icon: '/icons/statistic_3.svg',
+			},
+			{
+				label: 'Total Holders',
+				value: statistic.totalHolders,
+				icon: '/icons/statistic_4.svg',
+			},
+		];
+	}, [statistic]);
 
 	return (
 		<div className={styles['statistic-container']}>
 			<img
 				src='/images/roadmap_0.svg'
 				className={`w-[1.9169rem] h-[4.375rem] object-contain`}
+				alt='roadmap'
 			/>
 			<div className={styles['statistic-list']}>
 				{statisticBoxes.map((box, index) => (
@@ -50,13 +86,13 @@ function StatisticBox({
 	icon,
 }: {
 	label: string;
-	value: string;
+	value: number;
 	icon: string;
 }) {
 	return (
 		<div className={styles['statistic-box']}>
-			<img src={icon} className='w-[4.5rem] h-[4.5rem]' />
-			<p className={styles['statistic-box_value']}>{value}</p>
+			<img src={icon} className='w-[4.5rem] h-[4.5rem]' alt={icon} />
+			<p className={styles['statistic-box_value']}>{numberWithDot(value)}</p>
 			<p className={styles['statistic-box_label']}>{label}</p>
 		</div>
 	);
