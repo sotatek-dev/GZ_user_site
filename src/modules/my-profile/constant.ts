8e790844f9a37bec2610e809e38119c219e68a57
trongdev <hiiamtrong@gmail.com>
2022-10-06 09:39:26 +0700

    feat: update constant message my profile

---
+++ src/modules/my-profile/constant.ts
@@ -0,0 +1,32 @@
+const myProfileConstants = {
+	INVALID_EMAIL: 'Please enter a correct email, example "abc@mail.com"',
+	TRANSACTION_COMPLETED: 'Transaction Completed',
+	TRANSACTION_REJECTED: 'Transaction Rejected',
+	NETWORK_ERROR: 'Network Error!',
+
+	BUYINFO_MESSAGE_INCOMMING: 'Key can be mint when the dNFT sale round start',
+	BUYINFO_MESSAGE_AVAILABLE: 'Great! You are eligible to buy the key',
+	BUYINFO_MESSAGE_NOT_AVAILABLE: 'You are not elegible to buy this key',
+	BUYINFO_MESSAGE_NOT_ENOUGH_ROYALTY: "You don't have enough BUSD for royalty",
+	BUYINFO_MESSAGE_NOT_ENOUGH_BALANCE: "You don't have enough BNB/BUSD",
+
+	COUNTDOWN_INTIME: 'You can not buy key in',
+	COUNTDOWN_OUTTIME: 'You can buy key in',
+
+	DNFT_NO_ARMOR: 'No Armor',
+	DNFT_NO_SHOES: 'No Shoes',
+	DNFT_NO_GLOVES: 'No Gloves',
+	DNFT_NO_FUR: 'No Fur Color',
+	DNFT_NO_EYES: 'No Eyes Color',
+	DNFT_NO_UNIQUE_EYES: 'No Unique Eyes',
+	DNFT_NO_HELMENT: 'No Helmet',
+	DNFT_NO_HAIRSTYLE: 'No Hairstyle',
+	DNFT_NO_WEAPON: 'No Weapon/Accessory',
+	DNFT_NO_BACKGROUND: 'No Background',
+	DNFT_NO_FORCEFIELD: 'No Forcefield',
+	DNFT_NO_NATURE_POWER: 'No Nature Power',
+	DNFT_NO_COSMIC_POWER: 'No Cosmic Power',
+	DNFT_NO_SUMMONING_MASK: 'No Summoning Mask',
+};
+
+export default myProfileConstants;
