fe58722db71dbec3941bdd02fc71d03d5c78d7fe
Sotatek-MinhPhung <minh.phung@sotatek.com>
2022-10-11 15:44:13 +0700

    fix: change rescue to 10min for testing version

--- src/modules/common/helpers/isPublicSaleEnd.ts
+++ src/modules/common/helpers/isPublicSaleEnd.ts
@@ -1,9 +1,11 @@
 import BigNumber from 'bignumber.js';
-import { day, now } from 'common/constants/constants';
+import { minute, now } from 'common/constants/constants';
 
+// todo: change to 10min for testing version
 const isPublicSaleEnd = (publicSaleEndTime?: number) => {
 	return publicSaleEndTime
-		? new BigNumber(7).times(day).plus(publicSaleEndTime).lt(now())
+		? // ? new BigNumber(7).times(day).plus(publicSaleEndTime).lt(now())
+		  new BigNumber(10).times(minute).plus(publicSaleEndTime).lt(now())
 		: false;
 };
 
