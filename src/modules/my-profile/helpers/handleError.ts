8f58d061c5d40aca1989951fec377a8b8f16904b
trongdev <hiiamtrong@gmail.com>
2022-10-11 10:11:03 +0700

    fix: gz219 add show network error when buy key

---
+++ src/modules/my-profile/helpers/handleError.ts
@@ -0,0 +1,31 @@
+import { ErrorCode } from '@ethersproject/logger';
+import { ErrorMessage } from 'common/constants/error';
+import { handleCommonError, showError } from 'common/helpers/toast';
+
+export const handleBuyInfoError = (e?: any) => {
+	if (e?.code === ErrorCode.ACTION_REJECTED) {
+		showError(ErrorMessage.TRANSACTION_REJECTED);
+	} else if (
+		e.code === ErrorCode.SERVER_ERROR ||
+		e?.code === ErrorCode.TIMEOUT ||
+		e?.code === ErrorCode.UNKNOWN_ERROR
+	) {
+		showError(ErrorMessage.NETWORK_ERROR);
+	} else {
+		handleCommonError();
+	}
+};
+
+export const handleClaimError = (e?: any) => {
+	if (e?.code === ErrorCode.ACTION_REJECTED) {
+		showError(ErrorMessage.TRANSACTION_REJECTED);
+	} else if (
+		e.code === ErrorCode.SERVER_ERROR ||
+		e?.code === ErrorCode.TIMEOUT ||
+		e?.code === ErrorCode.UNKNOWN_ERROR
+	) {
+		showError(ErrorMessage.NETWORK_ERROR);
+	} else {
+		handleCommonError();
+	}
+};
