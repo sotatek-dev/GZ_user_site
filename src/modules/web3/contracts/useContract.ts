857f2316f9a37cb19f6e05284b94eb112c8193c6
Sotatek-MinhPhung <81953164+Sotatek-MinhPhung@users.noreply.github.com>
2022-10-13 16:49:08 +0700

    Feat/rescue (#100)
    fix: fetch data before connect wallet

--- src/modules/web3/contracts/useContract.ts
+++ src/modules/web3/contracts/useContract.ts
@@ -1,24 +1,33 @@
-import { BaseContract, ContractInterface, ethers } from 'ethers';
+import { BaseContract, Contract, ContractInterface, ethers } from 'ethers';
 import { useActiveWeb3React } from '../hooks/useActiveWeb3React';
 import { getContract } from '../helpers/getContract';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 export const useContract = <T extends BaseContract>(
 	abi: ContractInterface,
 	address: string
 ): T | null => {
+	const [defaultContract, setDefaultContract] = useState<any>();
 	const { library } = useActiveWeb3React();
 
-	return useMemo(() => {
+	const contract = useMemo(() => {
 		if (!ethers.utils.isAddress(address)) {
 			return null;
 		}
 
 		if (!library) {
-			// return getContract<T>(abi, address);
 			return null;
 		}
 
 		return getContract<T>(abi, address, library?.getSigner());
 	}, [abi, address, library]);
+
+	useEffect(() => {
+		if (window?.ethereum) {
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			setDefaultContract(new Contract(address, abi, provider));
+		}
+	}, []);
+
+	return contract || defaultContract;
 };
