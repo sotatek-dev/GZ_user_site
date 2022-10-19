32ef7ddd69514798b9c2e799b97df183acd357ee
Sotatek-MinhPhung <minh.phung@sotatek.com>
2022-10-19 10:10:21 +0700

    fix: temporary

--- src/modules/common/helpers/isPublicSaleEnd.ts
+++ src/modules/common/helpers/isPublicSaleEnd.ts
@@ -1,12 +1,15 @@
-import BigNumber from 'bignumber.js';
-import { minute, now } from 'common/constants/constants';
+// import BigNumber from 'bignumber.js';
+// import { day, now } from 'common/constants/constants';
 
-// todo: change to 10min for testing version
+// TODO: public sale end is true (temporary)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const isPublicSaleEnd = (publicSaleEndTime?: number) => {
-	return publicSaleEndTime
-		? // ? new BigNumber(7).times(day).plus(publicSaleEndTime).lt(now())
-		  new BigNumber(10).times(minute).plus(publicSaleEndTime).lt(now())
-		: false;
+	return true;
+
+	// return publicSaleEndTime
+	// 	? new BigNumber(7).times(day).plus(publicSaleEndTime).lt(now())
+	// 	: //   new BigNumber(10).times(minute).plus(publicSaleEndTime).lt(now())
+	// 	  false;
 };
 
 export default isPublicSaleEnd;
