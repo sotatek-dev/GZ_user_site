e0152c712decd9d4b9cfc5512196ed3b88730ab3
Sotatek-MinhPhung <minh.phung@sotatek.com>
2022-11-09 16:55:57 +0700

    fix: package

--- src/modules/common/helpers/isNftClaimable.ts
+++ src/modules/common/helpers/isNftClaimable.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import BigNumber from 'bignumber.js';
-import { now } from 'moment';
+import { now } from 'common/constants/constants';
 
 const isNftClaimable = (claimableTime: BigNumber.Value) => {
 	return new BigNumber(now()).gt(claimableTime);
