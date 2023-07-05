7c97f20e3a54365d46cd8210708f235d4ecbc858
Sotatek-HoangNguyen7 <hoang.nguyen7@sotatek.com>
2023-07-05 15:11:17 +0700

    v2 connect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,17 +12,12 @@ import { AuthProvider } from 'web3/contexts/authContext';
 import DocumentHead from 'common/components/head';
 import BigNumber from 'bignumber.js';
 
-import type { CoinbaseWallet } from '@web3-react/coinbase-wallet';
 import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core';
 import type { MetaMask } from '@web3-react/metamask';
 import type { Network } from '@web3-react/network';
 import type { WalletConnect } from '@web3-react/walletconnect';
 import type { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2';
 
-import {
-	coinbaseWallet,
-	hooks as coinbaseWalletHooks,
-} from '../connectors/coinbaseWallet';
 import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask';
 import { hooks as networkHooks, network } from '../connectors/network';
 import {
@@ -48,13 +43,12 @@ BigNumber.config({
 });
 
 const connectors: [
-	MetaMask | WalletConnect | WalletConnectV2 | CoinbaseWallet | Network,
+	MetaMask | WalletConnect | WalletConnectV2 | Network,
 	Web3ReactHooks
 ][] = [
 	[metaMask, metaMaskHooks],
 	[walletConnect, walletConnectHooks],
 	[walletConnectV2, walletConnectV2Hooks],
-	[coinbaseWallet, coinbaseWalletHooks],
 	[network, networkHooks],
 ];
 
