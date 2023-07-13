aa95a9f63391f0393dc4e17c1c7112035e4977a8
Hiep Nguyen Van <hiep.nguyen2@sotatek.com>
2023-07-13 14:55:28 +0700

    fix: zIndex

--- src/modules/merge-dnft/components/LockedMask/LockedMask.tsx
+++ src/modules/merge-dnft/components/LockedMask/LockedMask.tsx
@@ -2,8 +2,8 @@ import ImageBase from 'common/components/imageBase';
 
 export default function LockedMask() {
 	return (
-		<div className='card-nft-lock absolute inset-0 w-full flex justify-center items-center z-20 text-base font-semibold'>
-			<div className='w-full flex justify-center items-center gap-2'>
+		<div className='absolute inset-0 flex items-center justify-center w-full text-base font-semibold card-nft-lock z-[5]'>
+			<div className='flex items-center justify-center w-full gap-2'>
 				<ImageBase url='/icons/locked.svg' width={16} height={20} />
 				<span>LOCKED NFT</span>
 			</div>
